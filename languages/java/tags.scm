3f6c6ef3c5cdd6ead4fe7d32ec43e25c924c02af
Jason Lefever <jason.titus.lefever@gmail.com>
2025-08-11 14:20:15 -0400

    fix incorrect dependencies caused by incorrect comment ranges

--- languages/java/tags.scm
+++ languages/java/tags.scm
@@ -1,29 +1,34 @@
 (
   (block_comment)? @comment
+  .
   (annotation_type_declaration
     name: (identifier) @name) @tag.Annotation
 )
 
 (
   (block_comment)? @comment
+  .
   (constructor_declaration
     name: (identifier) @name) @tag.Constructor
 )
 
 (
   (block_comment)? @comment
+  .
   (class_declaration
     name: (identifier) @name) @tag.Class
 )
 
 (
   (block_comment)? @comment
+  .
   (enum_declaration
     name: (identifier) @name) @tag.Enum
 )
 
 (
   (block_comment)? @comment
+  .
   (field_declaration
     declarator: (variable_declarator
       name: (identifier) @name)) @tag.Field
@@ -31,18 +36,21 @@
 
 (
   (block_comment)? @comment
+  .
   (interface_declaration
     name: (identifier) @name) @tag.Interface
 )
 
 (
   (block_comment)? @comment
+  .
   (method_declaration
     name: (identifier) @name) @tag.Method
 )
 
 (
   (block_comment)? @comment
+  .
   (record_declaration
     name: (identifier) @name) @tag.Record
 )
